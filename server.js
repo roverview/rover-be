b853fc1cc907b2919951948d4b59a5e34dac212e
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-30 09:52:14 -0800

    db tables created working

--- server.js
+++ server.js
@@ -1,45 +1,44 @@
 'use strict'
 
-
-
-const pg = require('pg');
 const express = require('express');
+const pg = require('pg');
+const mps = require('make-promises-safe');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const connectionString = 'postgress://localhost:5432/'; //change this!  it is currently set for local machines!!!
+// const connectionString = process.env.DATABASE_URL;
+const connectionString = 'postgres://localhost:5432/roverview'; //change this!  it is currently set for local machines!!!
 const client = new pg.Client(connectionString);
 client.connect();
 
- app.use(express.static('./client'));
+// app.use(express.static('./client'));
 
- app.get('*', (req, res) => {
+app.get('*', (req, res) => {
     console.log('received request line 18');
     res.sendFile('index.html', {root: './client'});
     res.json({ msg: 'placeholder - deployment worked!' });
     console.log('file sent line 21');
 });
 
-
-
-app.get('/db/user', (req, res) => {
-    client.query(`SELECT * FROM user;`)
+app.get('/db/users', (req, res) => {
+    client.query(`SELECT * FROM users;`)
     .then(function(data) {
+        console.log('db/users route hit, sent users data!')
         res.send(data);
-        console.log('sent user data!')
     })
     .catch(function(err) {
         console.error(err);
     });
 });
-   
+
 //the image data base is not properly set up!!!!!(we might need to REFACTOR and create a JOIN to connect the users favorites)
 app.get('/db/image', function(req, res) {
     client.query(`
-        SELECT id, name,  image_id, image_src, 
-        FROM user
+        SELECT id, name, image_id, image_src 
+        FROM users
             JOIN image
-            ON user.id = image.user_id;
+            ON users.id = image.user_id;
     `)
     .then(function(data) {
         res.send(data);
@@ -52,25 +51,15 @@ app.get('/db/image', function(req, res) {
 
 //=================================================
 
-// these tables are not working correctly and throwing errors when I use the DB on my local machine.
-// createUserTable();
-// createImageTable();
-
-app.listen(PORT, () => {
-    console.log(`currently listening on ${PORT}`);
-});
-
-
-
-function createUserTable() {
+function createUsersTable() {
     client.query(`
-        CREATE TABLE IF NOT EXISTS user(
+        CREATE TABLE IF NOT EXISTS users(
             id SERIAL PRIMARY KEY,
-            name VARCHAR (256),
+            username VARCHAR (256),
             passphrase VARCHAR (256)
         );`
     )
-    .then(function(response) {
+    .then(function(res) { // changed to res instead of response for consistency
         console.log('created user table in db!')
     })
 }
@@ -83,10 +72,17 @@ function createImageTable() {
             camera_name VARCHAR (256),
             earth_date VARCHAR (256),
             img_src VARCHAR (256),
-            user_id VARCHAR (256),
+            user_id VARCHAR (256)
         );`
     )
-    .then(function(response) {
+    .then(function(res) { // changed to res instead of response for consistency
         console.log('created image table in db!')
     })
 }
+
+createUsersTable();
+createImageTable();
+
+app.listen(PORT, () => {
+    console.log(`currently listening on ${PORT}`);
+});
